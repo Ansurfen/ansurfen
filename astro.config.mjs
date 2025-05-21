b2e28d166e4e07df400f0c565b1c29ed4c4175c1
ansurfen <axf593161@gmail.com>
2025-05-21 23:04:10 +0800

    fix: try solve 404

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,7 +8,7 @@ import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://ansurfen.github.io',
+  site: 'https://ansurfen.github.io/ansurfen',
   integrations: [react(), mdx()],
   vite: {
     plugins: [tailwindcss()],
